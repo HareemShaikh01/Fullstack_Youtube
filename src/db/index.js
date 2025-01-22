f1687fa2eea10ae143360ff721e6cc7f6124f02a
HareemShaikh01 <hareemshaikh2006@gmail.com>
2025-01-22 21:18:53 +0500

    database connected

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const connectDB = async()=>{
+    try {
+       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}`)    
+       console.log("MONGODB CONNECTED SUCCESSFULLY");
+    } catch (error) {
+        console.log("MONGODB CONNECTION FAILED ",error);   
+        process.exit(1);
+    }
+}
+
+export default connectDB;
